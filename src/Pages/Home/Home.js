4cb555f4fbf1c8764400c05436826a332e3c95ac
Vicky <vickyfulare1998@gmail.com>
2024-10-08 11:39:00 +0530

    My commit

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Layout from '../../Components/Layouts/Layout'
+import Section1 from './Section1'
+import '../../Styles/HomeStyle.css'
+import Section2 from './Section2'
+import Section3 from './Section3'
+import Section4 from './Section4'
+import Section5 from './Section5'
+import Section6 from './Section6'
+import Section7 from './Section7'
+
+function Home() {
+  return (
+    <>
+    <Layout>
+      <Section1></Section1>
+      <Section2></Section2>
+      <Section3></Section3>
+      <Section4></Section4>
+      <Section5></Section5>
+      <Section6></Section6>
+      <Section7></Section7>
+    </Layout>
+    </>
+  )
+}
+
+export default Home
